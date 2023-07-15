e5369d58122dbfba79b56d17f6946583f0f659d3
DurgaPrasad237 <dangetidurgaprasad237@gmail.com>
2023-07-15 12:14:19 +0530

    copyarray

--- ArrayProblems/ArrayCopy.java
+++ ArrayProblems/ArrayCopy.java
@@ -1,8 +1,9 @@
 package ArrayProblems;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class MaxandMin {
+public class ArrayCopy {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -13,19 +14,14 @@ public class MaxandMin {
 		for(int i = 0; i<num.length; i++) {
 			num[i] = input.nextInt();
 		}
-		maxMin(num);
+		copy(num);
 	}
-	public static void maxMin(int arr[]) {
-		int max = arr[0];
-		int min = arr[0];
-		for(int i = 1; i<arr.length; i++) {
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-			else if(min > arr[i]) {
-				min = arr[i];
-			}		
+	public static void copy(int arr[]) {
+		int arr2[] = new int[arr.length];
+		for(int i = 0; i<arr.length; i++) {
+			arr2[i] = arr[i];
 		}
-		System.out.println("max :"+max+" min :"+min);
+		System.out.println(Arrays.toString(arr2));
 	}
+
 }
