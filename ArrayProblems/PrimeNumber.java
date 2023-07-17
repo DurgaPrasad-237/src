085925a29dd4cb94b958f3c4c5ca9af7d455eb5b
DurgaPrasad237 <dangetidurgaprasad237@gmail.com>
2023-07-17 23:38:41 +0530

    primenumber

---
+++ ArrayProblems/PrimeNumber.java
@@ -0,0 +1,26 @@
+package ArrayProblems;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter the number");
+		int num = input.nextInt();
+		int sum = 0;
+		for(int i = 2; i<=num; i++ ) {
+			if(num%i == 0)
+				sum++;
+		}
+		if(sum >=2) {
+			System.out.println("it is not a prime number");
+		}
+		else {
+			System.out.println("prime number");
+		}
+		
+	}
+	
+
+}
