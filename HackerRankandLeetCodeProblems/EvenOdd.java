b9753b96397543799b93188c80ccca9cbd3a81cc
DurgaPrasad237 <dangetidurgaprasad237@gmail.com>
2023-07-27 12:04:30 +0530

    evenodd

---
+++ HackerRankandLeetCodeProblems/EvenOdd.java
@@ -0,0 +1,20 @@
+package HackerRankandLeetCodeProblems;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter the number");
+		int num = input.nextInt();
+		evenodd(num);
+	}
+	public static void evenodd(int num) {
+		if(num%2 == 0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+	}
+
+}
