0c295653ee4ae9db66e974d819803eb4821997c7
DurgaPrasad237 <dangetidurgaprasad237@gmail.com>
2023-09-29 23:41:21 +0530

    duplicate

--- hackerRankandLeetCodeProblems/Duplicate.java
+++ hackerRankandLeetCodeProblems/Duplicate.java
@@ -16,16 +16,13 @@ public class Duplicate {
 		   System.out.println(dN(arr));
 	}
 	public static boolean dN(int nums[]) {
+		mergeSort(nums);
 		boolean result = false;
 		for(int i = 0; i<nums.length; i++) {
-			int sum = 1;
-			for(int j = i+1; j<nums.length; j++) {
-				if(nums[i] == nums[j]) {
-					sum++;
-					break;
-				}
+			if(i == nums.length-1) {
+				break;
 			}
-			if(sum > 1) {
+			if(nums[i] == nums[i+1]) {
 				result = true;
 				break;
 			}
@@ -33,5 +30,58 @@ public class Duplicate {
 		return result;
 		
 	}
+	
+	 public static void mergeSort(int array[]) {
+	    	if(array.length == 1) {
+	    		return;
+	    	}
+	    	int mid = array.length/2;
+	    	int left[] = new int[mid];
+	    	int right[] = new int[array.length-mid];
+	    	int j = 0;
+	    	for(int i = 0; i<array.length; i++) {
+	    		if(i<mid) {
+	    		left[i] = array[i];
+	    		}else {
+	    			right[j] = array[i];
+	    			j++;
+	    		}		
+	    	}
+	    	mergeSort(left);
+	    	mergeSort(right);
+	    	
+	    	merge(array,left,right);
+	    	
+	    }
+	    public static void merge(int array[], int left[], int right[]) {
+	    	int i = 0;
+	    	int j = 0;
+	    	int k = 0;
+	    	int ls = left.length;
+	    	int rs = right.length;
+	 
+	    	while (i < ls && j < rs) {
+	            if (left[i] <= right[j]) {
+	                array[k] = left[i];
+	                i++;
+	            } else {
+	                array[k] = right[j];
+	                j++;
+	            }
+	            k++;
+	        }
 
+	        while (i < ls) {
+	            array[k] = left[i];
+	            i++;
+	            k++;
+	        }
+
+	        while (j < rs) {
+	            array[k] = right[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	
 }
